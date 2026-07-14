bd3c849d777d722e8a97b58743ac47b472438cf7
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-07-14 11:22:15 +0700

    feat: add query for searching name, rating, year movies

---
+++ query1.sql
@@ -0,0 +1,7 @@
+SELECT "year"  FROM "movies" WHERE "year" > 2000; 
+
+SELECT  "last_name" FROM "actors" WHERE "last_name" LIKE '%s';
+
+SELECT "name", "rankscore" FROM "movies" WHERE "rankscore" BETWEEN 5 AND 7 AND "year" BETWEEN 2004 and 2007;
+
+SELECT COUNT("name") FROM "movies" WHERE "rankscore" = 6;
