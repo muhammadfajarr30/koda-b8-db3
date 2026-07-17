e6953a427ccaa903b29494cea2afa07f4b3809a7
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-07-17 14:28:27 +0700

    feat: enhance movie and actor queries to include additional fields

--- query1.sql
+++ query1.sql
@@ -1,6 +1,6 @@
-SELECT "year"  FROM "movies" WHERE "year" > 2000; 
+SELECT "year", "name"  FROM "movies" WHERE "year" > 2000;
 
-SELECT  "last_name" FROM "actors" WHERE "last_name" LIKE '%s';
+SELECT  "last_name","gender","id" FROM "actors" WHERE "last_name" LIKE '%s';
 
 SELECT "name", "rankscore" FROM "movies" WHERE "rankscore" BETWEEN 5 AND 7 AND "year" BETWEEN 2004 and 2007;
 
